8403e00eb5719a5abce0583ebc23000e46d0222e
Rodrigo Braga Prado <github@quati.com.br>
2022-06-18 20:43:12 -0300

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+BRANCH=$1
+DIR_SOURCE=$2
+SITE=$3
+RELEASE=$4
+BASE_DIR="/wwwroot/sites/"
+
+#RELEASE=$(date +%Y-%m-%d-%H-%M-%S)
+NEW_RELEASE="$BASE_DIR/$SITE/$BRANCH/$RELEASE"
+
+echo "mkdir -p $BASE_DIR/$SITE/$BRANCH/"
+echo "mv $DIR_SOURCE $NEW_RELEASE"
+
+echo "rodando as paradas tudo"
+
+echo "rodou remoto |=> $1 <= no direotrio => $2 <=| "
