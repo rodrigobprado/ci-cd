82066d2467b83b8945eeadb54239438c30680823
Rodrigo Braga Prado <github@quati.com.br>
2022-06-18 21:22:39 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,9 +9,11 @@ BASE_DIR="/wwwroot/sites"
 #NEW_RELEASE="$BASE_DIR/$SITE/$BRANCH/$RELEASE"
 NEW_RELEASE="$BASE_DIR/$SITE/$BRANCH/$RELEASE"
 
-echo "mkdir -p $BASE_DIR/$SITE/$BRANCH/"
-echo "mv $DIR_SOURCE $NEW_RELEASE"
+mkdir -p $BASE_DIR/$SITE/$BRANCH/
+mv $DIR_SOURCE $NEW_RELEASE
 
 echo "rodando as paradas tudo"
 
-echo "rodou remoto |=> $1 <= no direotrio => $2 <=| "
+echo "comandos e mais comandos"
+
+echo "fim do deploy"
