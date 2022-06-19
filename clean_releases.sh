3da7812b108dae9a03094c55bcd373362f337619
Rodrigo Braga Prado <github@quati.com.br>
2022-06-19 00:23:47 -0300

    Update clean_releases.sh

--- clean_releases.sh
+++ clean_releases.sh
@@ -7,6 +7,16 @@ RETENTION=$5
 BASE_DIR="/wwwroot/sites"
 BASE_DELETE="$BASE_DIR/$SITE/$BRANCH/releases/"
 
+echo "parte por parte"
+
+echo " $BASE_DELETE"
+echo "comando do ls"
+echo " $(ls $BASE_DELETE )"
+
+echo "o release tem $RELEASE"
+echo "a retenção tem $RETENTION"
+echo "a saida do filtrado do ls fica => $(ls $BASE_DELETE | sort -n |  awk '$1 < ($RELEASE - $RETENTION) {print ;}')"
+echo " "
 echo "o for ficara assim => for i in $(ls $BASE_DELETE | sort -n |  awk '$1 < ($RELEASE - $RETENTION) {print ;}'); do "
 
 for i in $(ls $BASE_DELETE | sort -n |  awk '$1 < ($RELEASE - $RETENTION) {print ;}'); do
